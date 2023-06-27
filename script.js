a16199f8ddb6f2dae2cc9b9f8f0e80ea698ef9e6
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-06-27 16:34:15 +0200

    foor loop to get 10 mails from endpoint

--- script.js
+++ script.js
@@ -3,9 +3,21 @@ console.log("Vue ok,", Vue);
 // adding endpoint with emails
 const endpoint = "https://flynn.boolean.careers/exercises/api/random/mail";
 
-const { createApp } = Vue;
-createApp({
+// Initializing vue
+const app = Vue.createApp({
   data() {
-    return {};
+    return {
+      emails: [],
+    };
   },
-}).mount("#root");
+  created() {
+    for (let i = 0; i < 10; i++) {
+      axios.get(endpoint).then((res) => {
+        this.emails.push(res.data.response);
+        console.log(this.emails);
+      });
+    }
+  },
+});
+
+app.mount("#root");
