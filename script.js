9fec1ac4666ec9e2f832010879bbd8f6d2f27ca2
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-06-27 16:03:48 +0200

    adding endpoint

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 console.log("Vue ok,", Vue);
 
+// adding endpoint with emails
+const endpoint = "https://flynn.boolean.careers/exercises/api/random/mail";
+
 const { createApp } = Vue;
 createApp({
   data() {
